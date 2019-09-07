7a162116f7021da4f96cf0a91e114c9d7eca7f1f
hothukurou <hothukurou@gmail.com>
2019-09-07 14:57:17 +0900

    initial commit

---
+++ Assets/Tapioca/Scripts/GameCrManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCrManager : MonoBehaviour
+{
+    public int score = 0;
+    public int tapioka_num = 0;
+    public GameObject NowScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ClickRankingButtonEvent()
+    {
+        // TODO: ここでscoreデータ送信を行う
+
+    }
+
+    public void ClickSearchNextBsuttonEvent()
+    {
+
+    }
+
+    // パチンコを打ちに行くボタン
+    public void ClickGoingToCRButtonEvent()
+    {
+
+    }
+
+    // タピオカを探しに行くボタン
+    public void ClickGoingToSearchTapiokaButtonEvent()
+    {
+
+    }
+
+    public void ClickTapiokaCup()
+    {
+
+    }
+
+
+}
---
+++ Assets/Tapioca/Scripts/GameFieldManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFieldManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void ClickSearchNextBsuttonEvent()
+    {
+
+    }
+
+    // パチンコを打ちに行くボタン
+    public void ClickGoingToCRButtonEvent()
+    {
+
+    }
+
+
+
+    public void ClickTapiokaCup()
+    {
+
+    }
+
+
+}
